3d803e846f56c667d192d47178290bc3335f9c8a
Matthew Steffen <matthew.steffen@gmail.com>
2020-12-31 10:59:12 -0800

    Debug and test update_minikube.sh

--- update_scripts/update_minikube.sh
+++ update_scripts/update_minikube.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-version="latest"
-if [[ "${1}" =~ --version=/* ]]; then
+# version=$(
+#   curl -s https://api.github.com/repos/kubernetes/minikube/releases/latest \
+#   | jq --raw-output '.name'
+# )
+if [[ "${1}" =~ --version=.* ]]; then
   version="${1##--version=}"
+  echo "Installing minikube version ${version}"
+else
+  version="latest"
+  echo "Installing latest minikube"
 fi
 
+set -x
 curl -L -o /tmp/minikube "https://storage.googleapis.com/minikube/releases/${version}/minikube-linux-amd64" \
   && chmod +x /tmp/minikube \
   && sudo mv /tmp/minikube /usr/local/bin/
