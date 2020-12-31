a3fb43f2b4f4cd6e2c32daff2afb3e801cc33d3c
Matthew Steffen <matthew.steffen@gmail.com>
2020-12-30 20:07:13 -0800

    Update script for kubectl

--- update_scripts/update_kubectl.sh
+++ update_scripts/update_kubectl.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+if ! sudo -n echo "hi"; then
+  echo "Error: must use sudo non-interactively; please run a sudo command"
+fi
+
 if [[ -z "$( which jq )" ]]; then
   echo -e "Error: must install 'jq'. Run:\n  sudo apt install jq"
   exit 1
@@ -13,11 +17,16 @@ if [[ -n "$(which kubectl)" ]]; then
 fi
 
 # Get latest kubectl release
-latest_release="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
-# latest_release=v1.15.5 # latest version that works with kubeflow
+kubectl_version="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
+# kubectl_version=v1.15.5 # latest version that works with kubeflow
+kubectl_tar=kubernetes-client-linux-amd64.tar.gz
 set +x
-echo "Getting latest kubectl release (${latest_release})"
+echo "Getting latest kubectl release (${kubectl_version})"
 set -x
-curl -L --progress-bar "https://storage.googleapis.com/kubernetes-release/release/${latest_release}/kubernetes-client-linux-amd64.tar.gz" \
-  | sudo tar -xvzf - --strip-components=3 -C /usr/local/bin "kubernetes/client/bin/kubectl"
+curl -L "https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/${kubectl_tar}" \
+  | sudo -n tar \
+           -xvzf - \
+           --strip-components=3 \
+           -C /usr/local/bin \
+           "kubernetes/client/bin/kubectl"
 sudo chmod +x /usr/local/bin/kubectl
