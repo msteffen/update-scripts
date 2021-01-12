e6c7e1735e627e4beebfc03774d359deb71487ed
Matthew Steffen <matthew.steffen@gmail.com>
2021-01-11 17:06:19 -0800

    Update & test update_kubebuilder.sh

--- update_kubebuilder.sh
+++ update_kubebuilder.sh
@@ -1,38 +1,61 @@
 #!/bin/bash
 
-set -ex
+set -e
 
+source_dir="$(dirname "${0}")"
+source "${source_dir}/add_to_file.sh"
+
+####
 # Parse flags
-version=latest
-eval "set -- $( getopt -l "version:" "--" "${0}" "${@:-}" )"
+####
+
+# Default values
+version="$(
+  curl https://api.github.com/repos/kubernetes-sigs/kubebuilder/releases \
+    | jq -r '.[].tag_name' \
+    | grep -v 'alpha\|beta\|rc' \
+    | sort -r --version-sort \
+    | sed -n '1p' \
+    | sed 's/^v//'
+)"
+destination_str='${HOME}/.local' # will be eval'ed, letting callers use e.g. $HOME
+
+# Run getopt in definition (rather than inline in 'eval') so that an error
+# (e.g. unrecognized argument) causes the script to exit.
+newargs="$( getopt -l "version:,destination:" "--" "${0}" "${@:-}" )"
+eval "set -- ${newargs}"
 while true; do
     case "${1}" in
         --version)
           version="${2#v}"
           shift 2
           ;;
+        --destination)
+          destination_str="${2}"
+          shift 2
+          ;;
         --)
           shift
           break
           ;;
-        *)
-          echo "Unrecognized argument: \"${1}\""
-          echo "Must be one of --state, --zone, --use-cloudfront, --no-pachyderm"
-          exit 1
-          ;;
     esac
 done
-
 
 os=$(go env GOOS)
 arch=$(go env GOARCH)
 
 # download kubebuilder and extract it to tmp
-curl -sL https://go.kubebuilder.io/dl/${version}/${os}/${arch} | tar -xz -C /tmp/
+destination="$(eval "cd \"${destination_str}\"" && pwd)"
+echo "Downloading kubebuilder for ${os}/${arch}, version ${version}"
+curl -sL https://go.kubebuilder.io/dl/${version}/${os}/${arch} | tar -xz -C /tmp
+mv "/tmp/kubebuilder_${version}_${os}_${arch}" "${destination}/kubebuilder"
+chmod +x "${destination}/kubebuilder/bin/kubebuilder"
 
-# move to a long-term location and put it on your path
 # (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
-if [[ -d /usr/local/kubebuilder ]]; then
-  sudo rm -rf /usr/local/kubebuilder
-fi
-sudo mv /tmp/kubebuilder_${version}_${os}_${arch} /usr/local/kubebuilder
+cmd="export KUBEBUILDER_ASSETS=\"${destination_str}/kubebuilder\""
+desc='KUBEBUILDER_ASSETS export'
+add_to_file "${cmd}" "${desc}" "${HOME}/.profile"
+
+cmd="export PATH=\"\${PATH}:${destination_str}/kubebuilder/bin\""
+desc='PATH export'
+add_to_file "${cmd}" "${desc}" "${HOME}/.profile"
