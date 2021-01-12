3c00360a43b5f790f235e3f0e0de4293d9a02624
Matthew Steffen <matthew.steffen@gmail.com>
2021-01-11 23:21:51 -0800

    Update update_minikube.sh

--- update_minikube.sh
+++ update_minikube.sh
@@ -2,19 +2,44 @@
 
 set -e
 
-# version=$(
-#   curl -s https://api.github.com/repos/kubernetes/minikube/releases/latest \
-#   | jq --raw-output '.name'
-# )
-if [[ "${1}" =~ --version=.* ]]; then
-  version="${1##--version=}"
-  echo "Installing minikube version ${version}"
-else
-  version="latest"
-  echo "Installing latest minikube"
-fi
+source_dir="$(dirname "${0}")"
+source "${source_dir}/add_to_file.sh"
 
-set -x
-curl -L -o /tmp/minikube "https://storage.googleapis.com/minikube/releases/${version}/minikube-linux-amd64" \
-  && chmod +x /tmp/minikube \
-  && sudo mv /tmp/minikube /usr/local/bin/
+####
+# Parse flags
+####
+
+# Default values
+version="$(
+  curl -sL https://api.github.com/repos/kubernetes/minikube/releases \
+    | jq -r '.[].tag_name' \
+    | grep -v 'alpha\|beta\|rc' \
+    | sort -r --version-sort \
+    | sed -n '1p' \
+    | sed 's/^v//'
+)"
+destination="${HOME}/.local/bin"
+
+# Run getopt in definition (rather than inline in 'eval') so that an error
+# (e.g. unrecognized argument) causes the script to exit.
+newargs="$( getopt -l "version:,destination:" "--" "${0}" "${@:-}" )"
+eval "set -- ${newargs}"
+while true; do
+    case "${1}" in
+        --version)
+          version="${2#v}"
+          shift 2
+          ;;
+        --destination)
+          destination="${2}"
+          shift 2
+          ;;
+        --)
+          shift
+          break
+          ;;
+    esac
+done
+
+curl -L -o "${destination}/minikube" "https://storage.googleapis.com/minikube/releases/v${version}/minikube-linux-amd64"
+chmod +x "${destination}/minikube"
