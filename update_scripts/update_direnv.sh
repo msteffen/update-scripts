22dcea518bbb038247af0374cc0591cedd6ecac1
Matthew Steffen <matthew.steffen@gmail.com>
2021-07-14 21:12:41 -0700

    Use add_to_file for direnv hook

--- update_scripts/update_direnv.sh
+++ update_scripts/update_direnv.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+set -e
+
+source_dir="$(dirname "${0}")"
+source "${source_dir}/add_to_file.sh"
+
 sudo apt install -y "direnv"
 sudo apt upgrade -y
 
 # Add direnv to bashrc
-echo "Maybe adding direnv hook to ${HOME}/.bashrc"
 cmd='eval "$(direnv hook bash)"'
-grep -q --fixed-strings "${cmd}" "${HOME}/.bashrc"&& {
-  echo "${HOME}/.bashrc is unchanged"
-} || {
-  echo -e "\n${cmd}" >>"${HOME}/.bashrc"
-  echo "added direnv hook to ${HOME}/.bashrc"
-}
+desc='direnv hook'
+add_to_file "${cmd}" "${desc}" "${HOME}/.bashrc"
